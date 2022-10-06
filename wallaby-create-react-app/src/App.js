c8275e83e20f574d699a5b101d376cdeafe31cff
inabernic <bernic.in@gmail.com>
2022-10-06 12:58:02 +0200

    initialisation of the DashboardPage

--- wallaby-create-react-app/src/App.js
+++ wallaby-create-react-app/src/App.js
@@ -1,11 +1,17 @@
+
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+// VIEWS -------------------------------------------
+import Dashboard from '../pages/Dashboard/Dashboard'
+
+// STYLE-------------------------------------------
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Routes>
+      <Route exact path="/" element={<Dashboard />} />
+    </Routes>
   );
 }
-
-export default App;
