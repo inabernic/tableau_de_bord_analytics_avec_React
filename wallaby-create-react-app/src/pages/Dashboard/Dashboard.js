67962a811c9ffdc126b97c60bebbd50ec1645830
inabernic <bernic.in@gmail.com>
2022-10-06 13:25:57 +0200

    initialisation of the Dashboard Page

---
+++ wallaby-create-react-app/src/pages/Dashboard/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+//STYLE
+import './Dashboard.css'
+
+//----------------------COMPONNETS--------------------
+
+
+export default function Dashboard() {
+    return (
+        <div className="container_page">
+            <p> cadre pour la page</p>
+        </div>
+    )
+}
