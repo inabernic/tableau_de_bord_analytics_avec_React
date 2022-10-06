ba55f3f561dace6e549c9ec1eeea74160e716d26
inabernic <bernic.in@gmail.com>
2022-10-06 18:10:47 +0200

    insert the logo SportSee

--- wallaby-create-react-app/src/pages/Dashboard/Dashboard.js
+++ wallaby-create-react-app/src/pages/Dashboard/Dashboard.js
@@ -9,7 +9,16 @@ import './Dashboard.css'
 export default function Dashboard() {
     return (
         <div className="container_page">
-            <div className="nav_horisontal"> cadre pour la page</div>
+            <div className="nav_horisontal"> cadre pour la page
+                <div className="logo">
+                    <img src="../assets/Logo.PNG" alt="logoSportSee" />
+                </div>
+                <div>Accueil</div>
+                <div>Profil</div>
+                <div>Réglage</div>
+                <div>Communauté</div>
+
+            </div>
             <div className="container_block">
                 <div className="nav_vertical"> les pitogrames</div>
                 <div className="container_info"> les grafiques</div>
