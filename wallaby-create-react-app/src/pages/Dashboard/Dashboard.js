21f9922251918d5c77d1aaeebe1cb8d0f1eb6e40
inabernic <bernic.in@gmail.com>
2022-10-07 11:03:06 +0200

    style of the nav bar

--- wallaby-create-react-app/src/pages/Dashboard/Dashboard.js
+++ wallaby-create-react-app/src/pages/Dashboard/Dashboard.js
@@ -9,14 +9,14 @@ import './Dashboard.css'
 export default function Dashboard() {
     return (
         <div className="container_page">
-            <div className="nav_horisontal"> cadre pour la page
-                <div className="logo">
+            <div className="nav_horisontal">
+                <div className="logo cursor_pointer">
                     <img src="../assets/Logo.PNG" alt="logoSportSee" />
                 </div>
-                <div>Accueil</div>
-                <div>Profil</div>
-                <div>Réglage</div>
-                <div>Communauté</div>
+                <div className='cursor_pointer'>Accueil</div>
+                <div className='cursor_pointer'>Profil</div>
+                <div className='cursor_pointer'>Réglage</div>
+                <div className='cursor_pointer'>Communauté</div>
 
             </div>
             <div className="container_block">
